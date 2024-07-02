5d62d2a078e662f6916b8b1b3cc9868146ccec92
giulianobre <giuliarnobre@gmail.com>
2024-07-02 08:44:41 -0300

    Criação da Api para Marca e Modelo

--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import AcessorioViewSet, CategoriaViewSet, CorViewSet, UserViewSet
+from core.views import AcessorioViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet, ModeloViewSet, UserViewSet
 
 router = DefaultRouter()
 
@@ -17,6 +17,8 @@ router.register(r"usuarios", UserViewSet, basename="usuarios")
 router.register(r"acessorios", AcessorioViewSet, basename="acesssorios")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
 router.register(r"cores", CorViewSet, basename="cores")
+router.register(r"marcas", MarcaViewSet, basename="marcas")
+router.register(r"modelos", ModeloViewSet, basename="modelos")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
